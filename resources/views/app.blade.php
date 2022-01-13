3b8e2946a7c688e21f5f4692c0d6013ce1a9ee44
edias15 <emerson15dias@hotmail.com>
2022-01-13 19:00:00 -0300

    meta noindex

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- meta for no robots -->
+        <meta name="robots" content="noindex, nofollow">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
