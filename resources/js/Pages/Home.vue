923ede41aee76edd46a19db9a34a6aa99acc48e1
edias15 <emerson15dias@hotmail.com>
2022-01-14 13:02:34 -0300

    set local cover photo

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -83,7 +83,7 @@
         </div>
     </div>
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1423666523292-b4…fHx8fGVufDB8fHx8&auto=format&fit=crop&w=2948&q=80" alt="" />
+        <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="http://siglacontabilidade.com.br/public/assets/woman-planning.jpg" alt="" />
     </div>
 </div>
 </template>
